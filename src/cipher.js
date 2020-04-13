17b7792005c85eebe9036e82b0cd736efdded5e9
jucilene-barros-silva <barrossilvajucilene@gmail.com>
2020-04-13 10:22:04 -0300

    commit do projeto, contendo os arquivos index.js, cipher.js, style.css, index.html e README

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,36 @@
 const cipher = {
-  // ...
+  encode: function (nOffset, message) {
+    if (typeof (nOffset) != typeof (0)) {
+      throw new TypeError();
+    } else if (typeof (message) != typeof ("a")){
+      throw new TypeError();
+    }
+  
+    let finalResultCipher = ""
+
+    for (let i = 0; i <message.length; i++){
+      let codigoDaLetraAscChipher = (((message[i].charCodeAt(0) - 65 + Number(nOffset)) % 26) + 65);
+      let resultCipher = String.fromCharCode(codigoDaLetraAscChipher);
+      finalResultCipher += resultCipher;
+    }
+
+    return finalResultCipher; 
+  },
+
+  decode: function (nOffset, message) {
+
+    let finalResultDecipher = ""
+
+    for (let i = 0; i <message.length; i++){
+      let codigoDaLetraAscDechipher = (((message[i].charCodeAt(0) - 90 - Number(nOffset)) % 26) + 90);
+      let resultDecipher = String.fromCharCode(codigoDaLetraAscDechipher);
+      finalResultDecipher += resultDecipher;
+          }
+    return finalResultDecipher;
+  },
+    
 };
 
 export default cipher;
+
+
--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+function parametersEncode (event){
+    event.preventDefault();
+    let nOffset = document.getElementById("offset").valueAsNumber;
+    let message = document.getElementById("write-text").value.toUpperCase();
+
+    document.getElementById("result").innerHTML = cipher.encode(nOffset,message);
+}
+
+function parametersDecode (event){
+    event.preventDefault();
+    let nOffset = document.getElementById("offset").valueAsNumber;
+    let message = document.getElementById("write-text").value.toUpperCase();
+
+    document.getElementById("result").innerHTML = cipher.decode(nOffset,message);
+       
+}
+
+document.getElementById("btnCipher").addEventListener("click", parametersEncode);
+document.getElementById("btnDecipher").addEventListener("click", parametersDecode);
+
+//Função para limpar e copiar não implementada
+/*function clearResult (event){
+    event.preventDefault();
+    let textResult = document.getElementById("result").innerHTML = cipher.decode(nOffset,message);
+    textResult.select();
+    document.execCommand("delete")
+}
+
+
+
+function copyResult (event){
+    event.preventDefault();
+    document.getElementById().select();
+    document.execCommand("copy");
+}
+
+document.getElementById("btnClear").addEventListener("click", clearResult);
+document.getElementById("btnCopy").addEventListener("click", copyResult);*/
