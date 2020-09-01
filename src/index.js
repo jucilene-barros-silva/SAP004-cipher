a4e9ee81ce7fa1c946d6ea18606234ee352423da
Jucilene Barros Silva <barrossilvajucilene@gmail.com>
2020-09-01 02:46:45 -0300

    Ajustando a identação do código

--- src/index.js
+++ src/index.js
@@ -1,31 +1,42 @@
-import cipher from './cipher.js';
+import cipher from "./cipher.js";
 
+function parametersEncode(event) {
+  event.preventDefault();
+  let nOffset = document.getElementById("offset").valueAsNumber;
+  let message = document.getElementById("write-text").value.toUpperCase();
 
-function parametersEncode (event){
-    event.preventDefault();
-    let nOffset = document.getElementById("offset").valueAsNumber;
-    let message = document.getElementById("write-text").value.toUpperCase();
-
-    document.getElementById("result-text").innerHTML = cipher.encode(nOffset,message);
+  document.getElementById("result-text").innerHTML = cipher.encode(
+    nOffset,
+    message
+  );
 }
 
-function parametersDecode (event){
-    event.preventDefault();
-    let nOffset = document.getElementById("offset").valueAsNumber;
-    let message = document.getElementById("write-text").value.toUpperCase();
+function parametersDecode(event) {
+  event.preventDefault();
+  let nOffset = document.getElementById("offset").valueAsNumber;
+  let message = document.getElementById("write-text").value.toUpperCase();
 
-    document.getElementById("result-text").innerHTML = cipher.decode(nOffset,message);
+  document.getElementById("result-text").innerHTML = cipher.decode(
+    nOffset,
+    message
+  );
 }
 
-document.getElementById("btn-cipher").addEventListener("click", parametersEncode);
-document.getElementById("btn-decipher").addEventListener("click", parametersDecode);
+document
+  .getElementById("btn-cipher")
+  .addEventListener("click", parametersEncode);
+document
+  .getElementById("btn-decipher")
+  .addEventListener("click", parametersDecode);
 
-document.getElementById("btn-clear").addEventListener("click",function clear(){
+document
+  .getElementById("btn-clear")
+  .addEventListener("click", function clear() {
     document.getElementById("write-text").value = "";
-    const result = document.getElementById("result-text").value = "";
-} );
+    const result = (document.getElementById("result-text").value = "");
+  });
 
-document.getElementById("btn-copy").addEventListener("click",function copy(){
-    const Copy = document.getElementById("result-text").select();
-    document.execCommand("Copy");
+document.getElementById("btn-copy").addEventListener("click", function copy() {
+  const Copy = document.getElementById("result-text").select();
+  document.execCommand("Copy");
 });
